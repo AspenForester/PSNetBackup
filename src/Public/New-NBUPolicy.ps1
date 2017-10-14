d2d519b6020059eaa63bdfca48de6ce9f8bfaa47
AspenForester <jblewis@gmail.com>
2017-10-14 11:04:29 -0500

    Initial ideas for New-NBUPolicy

---
+++ src/Public/New-NBUPolicy.ps1
@@ -0,0 +1,14 @@
+# bppolicynew - creates the framework
+# - just needs a policy name
+# - will fail if a policy with the same name alrewady exists.
+
+# bpplinfo - updates the attributes
+# - has about a thousand parameters!!!  And the dumbass didn't know to show which are mutually exclusive.
+
+# bpplclients - adds the clients
+
+# bpplinclude - adds the backup selection
+
+# bpplsched - adds the schedules
+
+# Whew!!!
