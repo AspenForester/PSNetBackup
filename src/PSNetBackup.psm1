69e56cc43a8201f0665107bb423960aab6752187
JB Lewis <jblewis@gmail.com>
2017-10-16 11:25:39 -0500

    Updated .psm1 to dot source from Public and Private

--- src/PSNetBackup.psm1
+++ src/PSNetBackup.psm1
@@ -1,5 +1,23 @@
-# Implement your module commands in this script.
+#Get public and private function definition files.
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
+#Dot source the files
+Foreach($import in @($Public + $Private))
+{
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
 
-# Export only the functions using PowerShell standard verb-noun naming.
-Export-ModuleMember -Function *-*
+# Here I might...
+# Read in or create an initial config file and variable
+# Export Public functions ($Public.BaseName) for WIP modules
+# Set variables visible to the module and its functions only
+
+Export-ModuleMember -Function $Public.Basename
