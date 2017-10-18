82d38d86683934cfd3331ca408d781e9d5acf594
JB Lewis <jblewis@gmail.com>
2017-10-18 15:41:50 -0500

    Now I saved the new approach to pasring the policy list

--- src/Public/Get-NBUPolicy.ps1
+++ src/Public/Get-NBUPolicy.ps1
@@ -32,7 +32,7 @@ Function Get-NetBackupPolicy
         $AllPolicies,
 
         [Parameter(ParameterSetName = "byPolicy",
-                   Mandatory = $true)]
+            Mandatory = $true)]
         [string]
         $Policy
         <#	[parameter(ParameterSetName = "hwos")]
@@ -56,7 +56,7 @@ Function Get-NetBackupPolicy
             Throw "This command needs to run on a master or media server"
         }
 
-        $IgnoreList = ('KEY','BCMD','RCMD','FOE','SHAREGROUP','SCHEDFOE','NAMES')
+        $IgnoreList = ('KEY', 'BCMD', 'RCMD', 'FOE', 'SHAREGROUP', 'SCHEDFOE', 'NAMES')
         $PolicyTypes = @{
             0  = 'Standard'
             1  = 'Proxy'
@@ -101,88 +101,88 @@ Function Get-NetBackupPolicy
 
                     $PolicyHash = @{}
 
-                        foreach ($Line in $bppllist)
+                    foreach ($Line in $bppllist)
+                    {
+                        $header = ($Line -split " ")[0]
+                        switch ($Header)
                         {
-                            $header = ($Line -split " ")[0]
-                            switch ($Header)
+                            {$_ -in $IgnoreList}
                             {
-                                {$_ -in $IgnoreList}
+                                # We're ignoring these! Might want to move the array
+                                break
+                            }
+                            {$_ -in ('CLASS', 'EOF')}
+                            {
+                                if ($InRecord -eq $True)
                                 {
-                                     # We're ignoring these! Might want to move the array
-                                     break
+                                    # You're about to start a new record, so output the one you've already compiled
+                                    [PSCustomObject]$PolicyHash
+                                    $PolicyHash = @{}
                                 }
-                                {$_ -in ('CLASS','EOF')}
-                                {
-                                    if ($InRecord -eq $True)
-                                    {
-                                        # You're about to start a new record, so output the one you've already compiled
-                                        [PSCustomObject]$PolicyHash
-                                        $PolicyHash = @{}
-                                    }
-                                        # Now Capture the stuff on this line
-                                        $Class = $Line -split ' '
-                                        $PolicyName = $Class[1]
-                                        $PolicyHash["PolicyName"] = $PolicyName
-                                        $InRecord = $True
-                                }
-                                'INFO'
-                                {
-                                    $info = $Line -split ' '
-                                    $PolicyHash["PolicyType"] = $PolicyTypes[$info[1]]
-                                    $PolicyHash["FollowNFS"] = $info[2]
-                                    $PolicyHash["ClientSideCompress"] = $info[3]
-                                    $PolicyHash["Priority"] = $info[4]
-                                    # Skipping Proxy Client
-                                    $PolicyHash["ClientSideEncryption"] = [bool]$info[6]
-                                    $PolicyHash["DisaterRecovery"] = [bool]$info[7]
-                                    $PolicyHash["MaxJobsPerClient"] = $info[8]
-                                    $PolicyHash["CrossMountPoints"] = [bool]$info[9]
-                                    # Skipping Field 10
-                                    $PolicyHash["Active"] = [bool]$info[11]
-                                    $PolicyHash["CollectTrueImageRestore"] = $info[12] # Triple State 0,1,2
-                                    $PolicyHash["BlockLevelIncremental"] = [bool]$info[13]
-                                }
-                                'RES' {}
-                                'POOL' {}
-                                'DATACLASSIFICATION' {}
-                                'CLIENT'
-                                {
-                                    # accumulate an array of Client objects
-                                }
-                                'INCLUDE'
-                                {
-                                    # accumulate an array of inclusion directives
-                                }
-                                'SCHED' {}
-                                'SCHEDWIN' {}
-                                'SCHEDRES' {}
-                                'SCHEDPOOL' {}
-                                'SCHEDRL' {}
-                                'SCHEDSG'
-                                {
-                                    # This is the end of a schedule object.
-                                }
+                                # Now Capture the stuff on this line
+                                $Class = $Line -split ' '
+                                $PolicyName = $Class[1]
+                                $PolicyHash["PolicyName"] = $PolicyName
+                                $InRecord = $True
+                            }
+                            'INFO'
+                            {
+                                $info = $Line -split ' '
+                                $PolicyHash["PolicyType"] = $PolicyTypes[$info[1]]
+                                $PolicyHash["FollowNFS"] = $info[2]
+                                $PolicyHash["ClientSideCompress"] = $info[3]
+                                $PolicyHash["Priority"] = $info[4]
+                                # Skipping Proxy Client
+                                $PolicyHash["ClientSideEncryption"] = [bool]$info[6]
+                                $PolicyHash["DisaterRecovery"] = [bool]$info[7]
+                                $PolicyHash["MaxJobsPerClient"] = $info[8]
+                                $PolicyHash["CrossMountPoints"] = [bool]$info[9]
+                                # Skipping Field 10
+                                $PolicyHash["Active"] = [bool]$info[11]
+                                $PolicyHash["CollectTrueImageRestore"] = $info[12] # Triple State 0,1,2
+                                $PolicyHash["BlockLevelIncremental"] = [bool]$info[13]
+                            }
+                            'RES' {}
+                            'POOL' {}
+                            'DATACLASSIFICATION' {}
+                            'CLIENT'
+                            {
+                                # accumulate an array of Client objects
+                            }
+                            'INCLUDE'
+                            {
+                                # accumulate an array of inclusion directives
+                            }
+                            'SCHED' {}
+                            'SCHEDWIN' {}
+                            'SCHEDRES' {}
+                            'SCHEDPOOL' {}
+                            'SCHEDRL' {}
+                            'SCHEDSG'
+                            {
+                                # This is the end of a schedule object.
+                            }
 
-                                Default
-                                {
-                                    # Need to save Default for reall errors
-                                }
+                            Default
+                            {
+                                # Need to save Default for reall errors
                             }
                         }
+                    }
 
 
-                        #http://www.symantec.com/business/support/index?page=content&id=HOWTO90333
-                        [pscustomobject] @{
-                            PolicyName = ($policy[0] -split " ")[0]
-                            CLASS      = ($policy[0] -split " ")
-                            NAMES      = ($policy[1] -split " ")
-                            INFO       = ($policy[2])[5..($policy[2].count)] -split " "
-                            KEY        = ($policy[3])[4..($policy[3].count)] -split " "
-                            BCMD       = ($policy[4])[5..($policy[4].count)] -split " "
-                            RCMD       = ($policy[5])[5..($policy[5].count)] -split " "
-                            RES        = ($policy[6])
-                            POOL       = ($policy[7])[5..($policy[7].count)] -split " "
-                        }
+                    #http://www.symantec.com/business/support/index?page=content&id=HOWTO90333
+                    [pscustomobject] @{
+                        PolicyName = ($policy[0] -split " ")[0]
+                        CLASS      = ($policy[0] -split " ")
+                        NAMES      = ($policy[1] -split " ")
+                        INFO       = ($policy[2])[5..($policy[2].count)] -split " "
+                        KEY        = ($policy[3])[4..($policy[3].count)] -split " "
+                        BCMD       = ($policy[4])[5..($policy[4].count)] -split " "
+                        RCMD       = ($policy[5])[5..($policy[5].count)] -split " "
+                        RES        = ($policy[6])
+                        POOL       = ($policy[7])[5..($policy[7].count)] -split " "
+                    }
 
                 }
                 'byPolicy'
